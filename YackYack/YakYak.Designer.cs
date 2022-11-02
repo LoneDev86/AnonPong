61e1e68a950494b7e33e88a737fa46060b371f8b
LoneDev86 <117284771+LoneDev86@users.noreply.github.com>
2022-11-02 09:29:25 -0400

    Add files via upload

---
+++ YackYack/YakYak.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace YackYack
+{
+    partial class YakYak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YakYak));
+            this.player = new System.Windows.Forms.PictureBox();
+            this.cpu = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerScore = new System.Windows.Forms.Label();
+            this.cpuLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player.Location = new System.Drawing.Point(12, 427);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(27, 127);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // cpu
+            // 
+            this.cpu.BackColor = System.Drawing.Color.Transparent;
+            this.cpu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("cpu.BackgroundImage")));
+            this.cpu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cpu.Location = new System.Drawing.Point(897, 230);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(27, 127);
+            this.cpu.TabIndex = 1;
+            this.cpu.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ball.BackgroundImage")));
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ball.Location = new System.Drawing.Point(434, 217);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(64, 48);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.BackColor = System.Drawing.Color.Lime;
+            this.playerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScore.Location = new System.Drawing.Point(105, 9);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(49, 32);
+            this.playerScore.TabIndex = 3;
+            this.playerScore.Text = "00";
+            // 
+            // cpuLabel
+            // 
+            this.cpuLabel.AutoSize = true;
+            this.cpuLabel.BackColor = System.Drawing.Color.Red;
+            this.cpuLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuLabel.Location = new System.Drawing.Point(735, 9);
+            this.cpuLabel.Name = "cpuLabel";
+            this.cpuLabel.Size = new System.Drawing.Size(49, 32);
+            this.cpuLabel.TabIndex = 4;
+            this.cpuLabel.Text = "00";
+            // 
+            // YakYak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(926, 702);
+            this.Controls.Add(this.cpuLabel);
+            this.Controls.Add(this.playerScore);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.player);
+            this.Name = "YakYak";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YakYak";
+            this.Load += new System.EventHandler(this.YakYak_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label cpuLabel;
+    }
+}
+
---
+++ YackYack/YakYak.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YackYack
+{
+    public partial class YakYak : Form
+    {
+        bool goup;
+        bool godown;
+        int speed = 15;
+        int ballx = 5;
+        int bally = 5;
+        int score = 0;
+        int cpuPoint = 0;
+
+        public YakYak()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private void YakYak_Load(object sender, EventArgs e)
+        {
+            Update();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+            
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            
+
+            playerScore.Text = "" + score;
+
+            cpuLabel.Text = "" + cpuPoint;
+
+            ball.Top -= bally;
+
+            ball.Left -= ballx;
+
+            cpu.Top += speed;
+            
+            if (score < 2)
+                
+            {
+                if (cpu.Top < 0 || cpu.Top > 455)
+
+                { 
+                    speed = -speed; 
+                }
+            }
+
+            else
+            {
+                cpu.Top = ball.Top + 15;
+            }
+
+            if (ball.Left < 0)
+            {
+                ball.Left = 434;
+
+                ballx = -ballx;
+                ballx += 5;
+                cpuPoint++;
+            }
+
+            if (ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = 434;
+                ballx = -ballx;
+                ballx += 5;
+                score++;
+            }
+
+            if (ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height)
+            {
+                bally = -bally;
+            }
+
+            if (ball.Bounds.IntersectsWith(player.Bounds) || ball.Bounds.IntersectsWith(cpu.Bounds))
+            {
+                ballx = -ballx;
+            }
+
+            if (goup == true && player.Top > 5)
+            {
+                player.Top -= 15;
+            }
+
+            if (godown == true && player.Top < 455)
+            {
+                player.Top += 15;
+            }
+
+            if(score > 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("You win this game");
+            }
+
+            if(cpuPoint > 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("CPU wins, you lose");
+            }
+
+        }
+    }
+}
